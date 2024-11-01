c968e69b7713d0aefe8f68aa6ea99742ad39aa7e
Tinakringen <tinakringen98@gmail.com>
2024-11-01 10:04:09 +0100

    forelesning 0111

---
+++ forelesning1510/reeeel.py
@@ -0,0 +1,12 @@
+print("Blackjack")
+
+player_chip_count = 100
+dealer_chip_count = 100
+
+print("You have " + {player_chip_count} + " chips")
+print("Dealer has " + {dealer_chip_count} + " chips")
+
+player_hand = []
+dealer_hand = []
+
+
